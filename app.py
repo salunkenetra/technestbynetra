784d043ef5789f91e8e2e7fcc54572a36d478c15
salunkenetra <nmsalunke_d25@el.vjti.ac.in>
2026-08-12 15:03:25 +0530

    Fix indentation for return statements in app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-return render_template('about.html')
+    return render_template('about.html')
 
 
 @app.route('/contact')
 def contact():
-return render_template('contact.html')
+    return render_template('contact.html')
 
 
 @app.route('/feedback', methods=['GET', 'POST'])
