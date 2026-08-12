a598354a71b2df57cf885e8708ca426db9352ba0
salunkenetra <nmsalunke_d25@el.vjti.ac.in>
2026-08-12 14:17:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,33 +3,37 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def home():
-return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
-return render_template("about.html")
+    return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route('/contact')
 def contact():
-return render_template("contact.html")
+    return render_template('contact.html')
 
 
-@app.route("/feedback", methods=["GET", "POST"])
+@app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
-if request.method == "POST":
-return render_template("thankyou.html")
 
-return render_template("feedback.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        print("Name:", name)
+        print("Email:", email)
+        print("Feedback:", message)
+
+        return render_template('thankyou.html')
+
+    return render_template('feedback.html')
 
 
-@app.route("/thankyou")
-def thankyou():
-    return render_template("thankyou.html")
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
